fb458a900746e45a7738ea7df42277afbfc7190c
M287312 <ernilsondaniel@sisconsultoria.com.br>
2022-08-16 16:07:35 -0300

    inicio

---
+++ src/main/java/br/com/sensosibre/SensosibreApplication.java
@@ -0,0 +1,13 @@
+package br.com.sensosibre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SensosibreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SensosibreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/sensosibre/SensosibreApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sensosibre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SensosibreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
